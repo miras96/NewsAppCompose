f5454ce740348af7796e8ffce40c145aef17bce9
Miras <miras96.96@inbox.ru>
2024-12-17 02:16:12 +0300

    NavigationGraph added

--- app/src/main/java/com/example/newsappcompose/models/presentation/Destinations.kt
+++ app/src/main/java/com/example/newsappcompose/models/presentation/Destinations.kt
@@ -1,9 +1,9 @@
 package com.example.newsappcompose.models.presentation
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.Home
-import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class Destinations(
@@ -20,13 +20,13 @@ sealed class Destinations(
     data object Favourite : Destinations(
         route = "favourite_screen",
         title = "Favorite",
-        icon = Icons.Outlined.Favorite
+        icon = Icons.Outlined.FavoriteBorder
     )
 
-    data object Notification : Destinations(
-        route = "notification_screen",
-        title = "Notification",
-        icon = Icons.Outlined.Notifications
+    data object Settings : Destinations(
+        route = "settings_screen",
+        title = "Settings",
+        icon = Icons.Outlined.Settings
     )
 
 }
--- app/src/main/java/com/example/newsappcompose/presentation/MainActivity.kt
+++ app/src/main/java/com/example/newsappcompose/presentation/MainActivity.kt
@@ -4,27 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.newsappcompose.models.presentation.Destinations
+import com.example.newsappcompose.presentation.screens.FavouriteScreen
+import com.example.newsappcompose.presentation.screens.HomeScreen
+import com.example.newsappcompose.presentation.screens.SettingsScreen
 import com.example.newsappcompose.presentation.theme.NewsAppComposeTheme
+import com.example.newsappcompose.presentation.widgets.BottomBarWidget
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,11 +29,25 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NewsAppComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController: NavHostController = rememberNavController()
+                val bottomBarHeight = 56.dp
+                val bottomBarOffsetHeightPx = remember { mutableStateOf(0f) }
+
+                var buttonsVisible = remember { mutableStateOf(true) }
+
+                Scaffold(
+                    bottomBar = {
+                        BottomBarWidget(
+                            navController = navController,
+                            state = buttonsVisible,
+                            modifier = Modifier
+                        )
+                    }) { paddingValues ->
+                    Box(
+                        modifier = Modifier.padding(paddingValues)
+                    ) {
+                        NavigationGraph(navController = navController)
+                    }
                 }
             }
         }
@@ -44,60 +55,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NewsAppComposeTheme {
-        Greeting("Android")
-    }
-}
-
-@Composable
-fun BottomBarWidget(
-    navController: NavHostController, state: MutableState<Boolean>, modifier: Modifier = Modifier
-) {
-    val screens = listOf(
-        Destinations.HomeScreen, Destinations.Favourite, Destinations.Notification
-    )
-
-    NavigationBar(
-        modifier = modifier,
-        containerColor = Color.LightGray,
-    ) {
-        val navBackStackEntry by navController.currentBackStackEntryAsState()
-        val currentRoute = navBackStackEntry?.destination?.route
-
-        screens.forEach { screen ->
-
-            NavigationBarItem(
-                label = {
-                    Text(text = screen.title!!)
-                },
-                icon = {
-                    Icon(imageVector = screen.icon!!, contentDescription = "")
-                },
-                selected = currentRoute == screen.route,
-                onClick = {
-                    navController.navigate(screen.route) {
-                        popUpTo(navController.graph.findStartDestination().id) {
-                            saveState = true
-                        }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
-                },
-                colors = NavigationBarItemDefaults.colors(
-                    unselectedTextColor = Color.Gray, selectedTextColor = Color.White
-                ),
-            )
+fun NavigationGraph(navController: NavHostController) {
+    NavHost(navController, startDestination = Destinations.HomeScreen.route) {
+        composable(Destinations.HomeScreen.route) {
+            HomeScreen()
+        }
+        composable(Destinations.Favourite.route) {
+            FavouriteScreen()
+        }
+        composable(Destinations.Settings.route) {
+            SettingsScreen()
         }
     }
-
 }
